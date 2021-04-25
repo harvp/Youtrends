5567ba38538ee4b66fb32778e793910fe74a2154
AndrewM <79992851+CompGameLP@users.noreply.github.com>
2021-04-25 16:49:05 -0400

    Attempt to go from array to string

--- topTenVids.php
+++ topTenVids.php
@@ -12,7 +12,7 @@
 	$outputStr = "<div class = \"flexContainerVert\">";
 	for($i = 0; $i < 10; $i++)
 	{
-		$outputStr .= "<div>" . $output[$i] . "</div>";
+		$outputStr .= "<div>" . $output[$i][0] . "</div>";
 	}
 	$outputStr .= "</div>";
 
