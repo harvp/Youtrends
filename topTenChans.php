2435ec277ce947cf36221765695f5f2f707f52a2
AndrewM <79992851+CompGameLP@users.noreply.github.com>
2021-04-25 16:45:17 -0400

    Remove engagement score remover

--- topTenChans.php
+++ topTenChans.php
@@ -9,10 +9,6 @@
 	//*/
 	$command = escapeshellcmd('python topchannels.py');
 	$output = json_decode(exec($command), true);
-	for($i = 0; $i < 10; $i++)
-	{
-		$output[$i] = substr($output[$i], 0, strripos($output[$i], " "));
-	}
 	$outputStr = "<div class = \"flexContainer\">";
 	for($i = 0; $i < 10; $i++)
 	{
